28a503574d380337388ff4a932543fdf18b34869
Alvaro Huertas <alvarohuertasgutierrez@gmail.com>
2023-02-13 23:15:40 -0500

    preEntrega

--- src/router/carts.router.js
+++ src/router/carts.router.js
@@ -1,20 +1,32 @@
 const {Router} = require ('express');
+const path = require('path')
+const ProductManager = require('../controller/productManager');
+const cart = path.join(__dirname, '../data/carts.json');
+const carts = new ProductManager(cart)
 const router = Router();
 
-router.get('/', async(req, res) => {
- 
-})
-router.get('/:id', (req, res) => {
-   
-})
+
 router.post('/', (req, res) => {
     //TODO
 })
-router.put('/', (req, res) => {
+
+router.get('/:id', (req, res) => {
+    try {
+        let cart = carts.getProductsById(parseInt(req.params.id));
+         if (cart) {
+          res.send({cart});
+        }
+        else {
+          res.send({error : "Producto no encontrado"});
+        } 
+      } catch (error) {
+        res.status(404).send({ error });
+      }
+})
+
+router.post('/:cid/products/:id', (req, res) => {
     //TODO
 })
-router.delete('/', (req, res) => {
-    //TODO
-})
+
 
 module.exports = router
